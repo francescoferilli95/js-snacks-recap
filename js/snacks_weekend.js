6c7346a6b42f8f5725504f2751b44805c0a90a56
francescoferilli95 <francesco.ferilli1995@libero.it>
2021-05-01 16:26:09 +0200

    adding first snack

---
+++ js/snacks_weekend.js
@@ -0,0 +1,34 @@
+/**
+ * 
+ *  SNACK: 1
+ *  A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l'iniziale maiuscola.
+ * 
+ */
+
+const strings = ['pippo','PLUTO','Paperino'];
+
+console.log(strings);
+
+const strings2 = strings.map((element, index) => { return element.charAt(0).toUpperCase() + strings[index].slice(1).toLowerCase()});
+
+console.log(strings2);
+
+
+
+
+
+/**
+ * 
+ *  SNACK: 2
+ *  Un oggetto rappresenta una finestra di un browser e contiene due campi: un array di tab aperte e un numero che indica l'indice della tab aperta nell'array
+ *  Il software deve guardare se c'è un social aperto ed eliminarlo dalle tab. nel caso la tab fosse attiva, deve attivare la successiva.
+ * 
+ */
+
+const browser = {
+    tab: ['Facebook','GitHub','Gmail'],
+    activeTab: 0,
+};
+
+console.log(browser);
+
